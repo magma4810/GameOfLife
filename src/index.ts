ca1cd95f6f94409753a4262243d7358980822ffe
Pavel <magma4810@gmail.com>
2025-02-01 01:22:26 +0300

    add jest config

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,6 @@ export function sum(x1: number, x2: number): number {
   return x1 + x2;
 }
 
-console.log(sum(4, 5));
-
 function main() {
   const mainHtmlElement: HTMLDivElement = document.createElement("div");
   const grid: HTMLDivElement = document.createElement("div");
