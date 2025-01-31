be19b0993f84b38c404e28928cf72c59ea8b6852
Pavel <magma4810@gmail.com>
2025-01-31 14:50:02 +0300

    init eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginJest from 'eslint-plugin-jest';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.test.ts'],
+    ...pluginJest.configs['flat/recommended'],
+  },
+  {
+    ignores: ["dist/","coverage/","webpack.config.js"]
+  }
+];
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+function sum(x1:number,x2:number) : number {
+    return x1+x2;
+}
+
+sum(4,5);
